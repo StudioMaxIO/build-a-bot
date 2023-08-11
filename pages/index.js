78b666861ef3229e5e1eaed0a4d2d350ee2bac78
Max Meier <max@studiomax.io>
2023-08-11 08:44:41 -0700

    remove test

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@
 import Chat from "../components/Chat";
 import React, { useState } from "react";
 import YourBot from "../bots/Template";
-import AssGPT from "../bots/Ass";
 // import WeatherBot from "../bots/Weather";
 // import WhetherBot from "../bots/Whether";
 
@@ -10,7 +9,6 @@ const ChatBot = ({}) => {
   const [view, setView] = useState("YourBot");
 
   const yourBot = new YourBot();
-  const assGPT = new AssGPT();
   // const weatherBot = new WeatherBot();
   // const whetherBot = new WhetherBot();
 
@@ -24,8 +22,6 @@ const ChatBot = ({}) => {
       case "WeatherBot":
          return <Chat chatBot={weatherBot} />;
       */
-      case "Ass":
-        return <Chat chatBot={assGPT} />;
       case "YourBot":
       default:
         return <Chat chatBot={yourBot} />;
@@ -41,9 +37,6 @@ const ChatBot = ({}) => {
         {/* <nav>
           <button onClick={() => setView("WhetherBot")}>Whether</button>
         </nav> */}
-        <nav>
-          <button onClick={() => setView("Ass")}>AssGPT</button>
-        </nav>
         <nav>
           <button onClick={() => setView("YourBot")}>Your Bot</button>
         </nav>
